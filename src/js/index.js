a9b60c4f2a5e17aeafe4ac3c637d061005a4bc70
Kyle Hogan <khogan@movableink.com>
2018-03-16 17:32:41 -0400

    refactored javascript and style

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,82 @@
 import $ from "jquery";
-import "../css/style.css";
-import "../css/style2.scss";
-import params from "./params";
+import "../css/style.scss";
 import WebviewTest from "./webview-test";
+
+window.$ = $;
 
 $("#get_source").submit(function(event){
   event.preventDefault();
   getWebview($("#source_url").val());
 });
 
+$("#add_custom_param").submit(function(event){
+    
+  
+  
+  
+        event.preventDefault();
+  let input = inputTemplate($("#custom_param_name").val(),"");
+  $("#params_adjuster .text_inputs").append(input);
+    $(".param_input_container").fadeIn();
+});
+
 function getWebview(url) {
 
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", `http://cors.movableink.com/${url}`, true);
+  xhr.open("GET", proxyUrl(url), true);
   xhr.onload = event => {
-
-    window.webviewTest = new WebviewTest(xhr.responseText);
-    $("#webview_container").append(webviewTest.doc);
-    for(let param in webviewTest.allParams){
-      $("#params_adjuster").prepend(`<div><label for="${param}">${param}</label><input type="text" name=${param} id="param-input-${param}" value=${webviewTest.allParams[param]}/></div>`);
-    }
-    $("#params_adjuster").submit(webviewTest.updateImageSources);
-
+    handleWebview(xhr.responseText);
   };
-
   xhr.send();
+
+}
+
+function handleWebview(responseText){
+
+  $("#webview_container").empty();
+  $("#params_adjuster .param_input_container").remove();
+  const webviewTest = new WebviewTest(responseText);
+  $("#webview_container").append(webviewTest.doc);
+
+  $("#params_adjuster").show();
+  $("#add_custom_param").show();
+  $(".form_container").show();
+  
+  for(let param in webviewTest.allParams){
+    let input = inputTemplate(param,webviewTest.allParams[param]);
+    $("#params_adjuster .text_inputs").append(input);
+    $(".param_input_container").fadeIn();
+  }
+
+  $("#params_adjuster").submit(function(event){
+    event.preventDefault();
+    const queryString = $(this).serialize();
+    webviewTest.updateImageSources(queryString);
+  });
+}
+
+function proxyUrl(url) {
+
+  const a = document.createElement('a');
+  a.href = url;
+
+  return [
+    "https://cors-anywhere.herokuapp.com/",
+    // "https://cors.movableink.com/",
+    a.hostname,
+    a.pathname,
+    a.search,
+    a.hash
+  ].join("");
+
+}
+
+function inputTemplate(param,value){
+
+  const inputContainer = $(`<div class="param_input_container"><span class="input_label" for="${param}">${param}</span><input type="text" name=${param} value="${value}"/><div class="kill"></div></div>`);
+  $(".kill",inputContainer).click(function(x){
+    inputContainer.remove();
+  });
+  return inputContainer;
 
 }
--- src/js/webview-test.js
+++ src/js/webview-test.js
@@ -1,61 +1,68 @@
 import $ from "jquery";
-import getParams from "./params";
 
-export default class WebviewTest{
-  constructor(raw){
+export default class WebviewTest {
+
+  constructor(raw) {
+
     this.raw = raw;
     this.doc = $(raw);
     this.miContent = this.findMiContent(this.doc);
-    this.allParams = this.reduceParams(this.miContent);
+    this.allParams = this.collectParams(this.miContent);
     this.updateImageSources = this.updateImageSources.bind(this);
+
   }
 
-  updateImageSources(event){
-    event.preventDefault();
-    let qs = $("#params_adjuster").serialize();
-    this.miContent.forEach((img,i)=>{
+  updateImageSources(queryString) {
+
+    this.miContent.forEach((img, i) => {
       let src = img.attr("src");
-      img.attr("src",src.split("?")[0] + "?" + qs);
+      img.attr("src", src.split("?")[0] + "?" + queryString);
     });
+
   }
 
-  findMiContent(doc){
+
+  findMiContent(doc) {
+
     let miContent = [];
-    const everyImage = $("img",doc);
-    everyImage.each(function(i,elem){
+    const everyImage = $("img", doc);
+    everyImage.each(function (i, elem) {
       let img = $(this);
       let src = img.attr("src");
-      let params = getParams(src);
-      img.data({params});
-      if(params.hasOwnProperty("mi_u") || src.indexOf("aejohg.com") > -1){
+      if (src.indexOf("mi_u=") > -1 || src.indexOf("aejohg.com") > -1) {
         miContent.push(img)
       }
     });
     return miContent;
+
   }
 
-  reduceParams(images){
+  collectParams(images) {
+
     let allParams = {};
 
-    for(let i = 0; i< images.length; i++){
-      let params = images[i].data("params");
-      allParams = Object.assign(allParams,params);
+    for (let i = 0; i < images.length; i++) {
+      let params = this.paramsFromUrl(images[i].attr("src"));
+      allParams = Object.assign(allParams, params);
     }
 
     return allParams;
+
   }
-  //
-  // makeInputs(params){
-  //   let paramInputs = {};
-  //   for(let p in params){
-  //     this.appendInput(p,params[p]);
-  //     paramInputs[p] = $(`#param-input-${p}`);
-  //     paramInputs[p].val(params[p]);
-  //   }
-  //   this.paramInputs = paramInputs;
-  // }
-  //
-  // appendInput(param,value){
-  //   $("#params_adjuster").prepend(`<div><label for="${param}">${param}</label><input type="text" name=${param} id="param-input-${param}" value=${value}/></div>`);
-  // }
+
+  paramsFromUrl(url) {
+
+    if (!url) url = "";
+    const _searchString = url.split("?")[1] || "";
+    const _urlParams = {};
+    const search = /([^&=]+)=?([^&]*)/g;
+
+    let match = search.exec(_searchString);
+    while (match) {
+      _urlParams[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
+      match = search.exec(_searchString);
+    }
+    return _urlParams;
+
+  }
 }
